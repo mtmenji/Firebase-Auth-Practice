0458b9ee2ae479e82f3502e5468547ca4af343db
mtmenji <mtmenji@gmail.com>
2024-05-23 11:58:25 -0600

    Practicing page routing

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    console.log('Rendering About Page');
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+    console.log('Rendering Contact Page');
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Contact us at contact@example.com.</p>
+    </div>
+  );
+}
+
+export default Contact;
